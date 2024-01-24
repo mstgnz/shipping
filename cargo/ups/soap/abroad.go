45546ac3a7ca6b3731b5a9fce757556c7e798bdf
mstgnz <genezmesut@gmail.com>
2024-01-24 23:43:18 +0300

    ups cargo struct

--- cargo/ups/soap/abroad.go
+++ cargo/ups/soap/abroad.go
@@ -1,1 +1,172 @@
 package soap
+
+type shipAccept struct {
+	AccessRequest         accessRequest
+	ShipmentAcceptRequest shipmentAcceptRequest
+}
+
+type shipConfirm struct {
+	AccessRequest          accessRequest
+	ShipmentConfirmRequest shipmentConfirmRequest
+}
+
+type accessRequest struct {
+	AccessLicenseNumber string
+	UserId              string
+	Password            string
+}
+
+type shipmentAcceptRequest struct {
+	Request        request
+	ShipmentDigest string
+}
+
+type shipmentConfirmRequest struct {
+	Request  request
+	Shipment shipment
+}
+
+type request struct {
+	RequestAction        string
+	RequestOption        string
+	TransactionReference transactionReference
+}
+
+type transactionReference struct {
+	CustomerContext string
+}
+
+type shipment struct {
+	Description        string
+	Shipper            shipper
+	ShipFrom           shipFrom
+	ShipTo             shipTo
+	PaymentInformation paymentInformation
+	Service            service
+	Package            shipmentPackage
+}
+
+type shipper struct {
+	Name                    string
+	AttentionName           string
+	PhoneNumber             string
+	TaxIdentificationNumber string
+	ShipperNumber           string
+	Address                 address
+}
+
+type address struct {
+	AddressLine1      string
+	AddressLine2      string
+	AddressLine3      string
+	City              string
+	PostalCode        string
+	CountryCode       string
+	StateProvinceCode string
+}
+
+type shipFrom struct {
+	CompanyName             string
+	AttentionName           string
+	PhoneNumber             string
+	TaxIdentificationNumber string
+	Address                 address
+}
+
+type shipTo struct {
+	CompanyName   string
+	AttentionName string
+	PhoneNumber   string
+	Address       address
+}
+
+type paymentInformation struct {
+	Prepaid prepaid
+}
+
+type prepaid struct {
+	BillShipper billShipper
+}
+
+type billShipper struct {
+	AccountNumber string
+}
+
+type service struct {
+	Code        string
+	Description string
+}
+
+type shipmentPackage struct {
+	Description   string
+	PackagingType packagingType
+	PackageWeight packageWeight
+	Dimensions    dimensions
+}
+
+type packagingType struct {
+	Code        string
+	Description string
+}
+
+type packageWeight struct {
+	Weight            string
+	UnitOfMeasurement unitOfMeasurement
+}
+
+type unitOfMeasurement struct {
+	Code string
+}
+
+type dimensions struct {
+	Length            string
+	Width             string
+	Height            string
+	UnitOfMeasurement unitOfMeasurement
+}
+
+type labelRecovery struct {
+	AccessRequest        accessRequest
+	LabelRecoveryRequest labelRecoveryRequest
+}
+
+type labelRecoveryRequest struct {
+	Request            request
+	LabelSpecification labelSpecification
+	LabelDelivery      labelDelivery
+	TrackingNumber     string
+}
+
+type labelSpecification struct {
+	HTTPUserAgent    string
+	LabelImageFormat labelImageFormat
+}
+
+type labelImageFormat struct {
+	Code string
+}
+
+type labelDelivery struct {
+	LabelLinkIndicator   string
+	ResendEMailIndicator string
+}
+
+type shipCancel struct {
+	AccessRequest       accessRequest
+	VoidShipmentRequest voidShipmentRequest
+}
+
+type voidShipmentRequest struct {
+	Request                      request
+	ShipmentIdentificationNumber string
+}
+
+type shipTracking struct {
+	AccessRequest accessRequest
+	TrackRequest  trackRequest
+}
+
+type trackRequest struct {
+	Request        request
+	TrackingNumber string
+}
--- cargo/ups/soap/domestic.go
+++ cargo/ups/soap/domestic.go
@@ -1,1 +1,71 @@
 package soap
+
+type loginData struct {
+	CustomerNumber string
+	UserName       string
+	Password       string
+}
+
+type createData struct {
+	SessionID        string
+	ShipmentInfo     shipmentInfo
+	ReturnLabelLink  bool
+	ReturnLabelImage bool
+}
+
+type shipmentInfo struct {
+	ShipperName                string
+	ShipperAddress             string
+	ShipperCityCode            string
+	ShipperAreaCode            string
+	ShipperMobilePhoneNumber   string
+	ShipperEMail               string
+	ConsigneeName              string
+	ConsigneeAddress           string
+	ConsigneeCityCode          string
+	ConsigneeAreaCode          string
+	ConsigneeMobilePhoneNumber string
+	ConsigneeEMail             string
+	PaymentType                string
+	SmsToConsignee             string
+	ShipperAccountNumber       string
+	ShipperContactName         string
+	ShipperPostalCode          string
+	ShipperPhoneExtension      string
+	ShipperPhoneNumber         string
+	ShipperExpenseCode         string
+	ConsigneeAccountNumber     string
+	ConsigneeContactName       string
+	ConsigneeExpenseCode       string
+	ConsigneePostalCode        string
+	ConsigneePhoneNumber       string
+	CustomerReferance          string
+	CustomerInvoiceNumber      string
+	DescriptionOfGoods         string
+	DeliveryNotificationEmail  string
+	NumberOfPackages           string
+	PackageType                string
+	ServiceLevel               string
+	IdControlFlag              string
+	PhonePrealertFlag          string
+	InsuranceValue             string
+	InsuranceValueCurrency     string
+	ValueOfGoods               string
+	ValueOfGoodsCurrency       string
+	ValueOfGoodsPaymentType    string
+	ThirdPartyAccountNumber    string
+	ThirdPartyExpenseCode      string
+	SmsToShipper               string
+}
+
+type cancelData struct {
+	sessionId     string
+	customerCode  string
+	waybillNumber string
+}
+
+type trackingData struct {
+	SessionID        string
+	InformationLevel string
+	TrackingNumber   string
+}
