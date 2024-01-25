7eda45d55f739fe9fb970dbbfc7177025ab77417
mstgnz <genezmesut@gmail.com>
2024-01-26 00:03:29 +0300

    struct

--- config/shipper.go
+++ config/shipper.go
@@ -19,11 +19,20 @@ type Shipper interface {
 	GetMode() string
 	SetMode(mode string)
 
+	GetDomestic() bool
+	SetDomestic(isDomestic bool)
+
+	GetCurrentCredential() string
+	SetCurrentCredential(currentCredential string)
+
+	GetCurrentEndpoint() string
+	SetCurrentEndpoint(currentEndpoint string)
+
 	GetCredentials() []Credential
 	GetCredential(title string) Credential
 	AddCredential(title, username, password string)
 	DelCredential(title string) []Credential
-	
+
 	CreateCargo(data map[string]any) (map[string]any, error)
 
 	WhereIsTheCargo(tracking string) (map[string]any, error)
