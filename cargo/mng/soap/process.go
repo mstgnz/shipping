e3088db4e6eda63e01fdc00370f49db85025a863
mstgnz <genezmesut@gmail.com>
2024-01-28 16:24:38 +0300

    add comment

--- cargo/mng/soap/process.go
+++ cargo/mng/soap/process.go
@@ -1,41 +1,50 @@
 package soap
 
 import (
+	"bytes"
 	"encoding/xml"
 	"errors"
+	"log"
+	"net/http"
 
 	"github.com/mstgnz/shipping/config"
 )
 
-// CreateAbroad
-// https://service.mngkargo.com.tr/musterikargosiparis/musterikargosiparis.asmx
+// CreateAbroad global service
 func CreateAbroad(current cargo.Current, data cargo.ShippingData) (map[string]any, error) {
-	_ = parseAbroad(data)
+	_, _ = parseAbroad(data)
 	return nil, nil
 }
 
+// CreateDomestic local service
+// TEST REQUEST: https://service.mngkargo.com.tr/tservis/musterikargosiparis.asmx?WSDL
+// LIVE REQUEST: https://service.mngkargo.com.tr/musterikargosiparis/musterikargosiparis.asmx?WSDL
 func CreateDomestic(current cargo.Current, data cargo.ShippingData) (map[string]any, error) {
 	xmlData, err := parseDomestic(data)
 	if err != nil {
 		return nil, err
 	}
+
+	resp, err := http.Post(current.Endpoint.GetDevelopment(), "application/x-www-form-urlencoded", bytes.NewBuffer(xmlData))
+
+	log.Println(resp)
 	result := make(map[string]interface{})
 	result["xml"] = xmlData
 	return result, nil
 }
 
-func parseAbroad(data cargo.ShippingData) error {
-	return nil
+func parseAbroad(data cargo.ShippingData) ([]byte, error) {
+	return nil, nil
 }
 
-func parseDomestic(data cargo.ShippingData) (string, error) {
+func parseDomestic(data cargo.ShippingData) ([]byte, error) {
 	data, ok := data.(SiparisGirisiDetayliV3)
 	if ok {
 		xmlData, err := xml.MarshalIndent(data, "", "    ")
 		if err != nil {
-			return "", err
+			return nil, err
 		}
-		return string(xmlData), nil
+		return xmlData, nil
 	}
-	return "", errors.New("data parameter not a `SiparisGirisiDetayliV3` type")
+	return nil, errors.New("data parameter not a `SiparisGirisiDetayliV3` type")
 }
