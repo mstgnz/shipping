1474e67ba6e7daecf9c3dc15c67531d6ad5f9528
mstgnz <genezmesut@gmail.com>
2024-01-31 17:39:18 +0300

    mng xml header

--- cargo/mng/soap/process.go
+++ cargo/mng/soap/process.go
@@ -31,9 +31,7 @@ func CreateDomestic(current cargo.Current, data cargo.ShippingData) (*cargo.Resp
 	orderData.WsPassword = current.Credential.GetPassword()
 	orderData.PSifre = current.Credential.GetPassword()
 
-	xmlHeader := `<?xml version="1.0" encoding="utf-8"?>
-<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
-  <soap:Body>`
+	xmlHeader := `<?xml version="1.0" encoding="utf-8"?><soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"><soap:Body>`
 
 	xmlFooter := `</soap:Body></soap:Envelope>`
 
