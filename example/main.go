67b2ec9878277641cdd334c4851c2dd0377d6fbb
mstgnz <genezmesut@gmail.com>
2024-02-02 00:04:13 +0300

    example

--- example/main.go
+++ example/main.go
@@ -1,5 +1,18 @@
 package main
 
 func main() {
+
+	// ExampleAras()
+	// ExampleDhl()
+	// ExampleFedex()
+	// ExampleIyi()
 	ExampleMng()
+	// ExamplePtt()
+	// ExampleSendeo()
+	// ExampleSurat()
+	// ExampleTnt()
+	// ExampleTurk()
+	// ExampleUps()
+	// ExampleVatan()
+	// ExampleYurtici()
 }
